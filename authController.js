24d431cb95d46fc60f30c34b9e3b834dda5060ac
Samadhan Badgujar <140482455+SamBadgujar@users.noreply.github.com>
2024-11-13 12:40:16 +0530

    Add files via upload

--- authController.js
+++ authController.js
@@ -1,6 +1,4 @@
-//const bcrypt = require("bcrypt");
-const bcrypt = require("bcrypt");
-const User = require("../Models/User");
+const User = require("../models/User");
 const jwt = require("jsonwebtoken");
 
 const register = async (req, res) => {
@@ -24,21 +22,20 @@ const login = async (req, res) => {
   try {
     const user = await User.findOne({ email });
     if (!user) {
-      return res.status(404).json({ message: "User not found" });
-    }
-
-    const isPasswordCorrect = await bcrypt.compare(password, user.password);
-    if (!isPasswordCorrect) {
       return res.status(400).json({ message: "Invalid credentials" });
     }
 
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+    const isMatch = await user.comparePassword(password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
       expiresIn: "1h",
     });
-    res.status(200).json({ token });
+    res.json({ token });
   } catch (error) {
-    console.error("Login route error:", error);
-    res.status(500).json({ message: "Internal Server Error" });
+    res.status(500).json({ message: "Server error" });
   }
 };
 
